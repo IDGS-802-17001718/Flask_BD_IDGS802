2aa7b14eb8267b1c1d52d212c96b70633285d7d1
MarioVargas-C <mario.vargasbuss@gmail.com>
2024-02-29 21:06:03 -0600

    LMVP ABC Completo 29/02/24

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template, Response, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 from flask import flash
 from flask import g
@@ -27,6 +27,47 @@ def index():
         db.session.commit()
     return render_template("index.html", form=alum_form)
 
+@app.route("/eliminar", methods=["GET", "POST"])
+def eliminar():
+    alum_form=forms.UserForm2(request.form)
+
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        """ alum=Alumnos.query.filter_by(id=id).delete() """
+        alum_form.id.data=alum.id
+        alum_form.nombre.data=alum.nombre
+        alum_form.apaterno.data=alum.apaterno
+        alum_form.email.data=alum.email
+    if request.method=='POST':
+        id=alum_form.id.data
+        alum=Alumnos.query.get(id)
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('ABC_Completo'))
+    return render_template("eliminar.html", form=alum_form)
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+    alum_form=forms.UserForm2(request.form)
+
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        """ alum=Alumnos.query.filter_by(id=id).delete() """
+        alum_form.id.data=alum.id
+        alum_form.nombre.data=alum.nombre
+        alum_form.apaterno.data=alum.apaterno
+        alum_form.email.data=alum.email
+    if request.method=='POST':
+        id=alum_form.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre=alum_form.nombre.data
+        alum.apaterno=alum_form.apaterno.data
+        alum.email=alum_form.email.data
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('ABC_Completo'))
+    return render_template("modificar.html", form=alum_form)
 @app.route("/ABC_Completo", methods=["GET", "POST"])
 def ABDCompleto():
     create_form=forms.UserForm2(request.form)
